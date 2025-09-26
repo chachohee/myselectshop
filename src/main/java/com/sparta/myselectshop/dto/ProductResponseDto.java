8c390e00a2f1727710cefe7e020da90e381f2351
chachohee <cxoxee@gmail.com>
2025-09-27 01:23:43 +0900

    상품 폴더 설계 & 폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/dto/ProductResponseDto.java
+++ src/main/java/com/sparta/myselectshop/dto/ProductResponseDto.java
@@ -1,8 +1,12 @@
 package com.sparta.myselectshop.dto;
 
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.ProductFolder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -13,6 +17,7 @@ public class ProductResponseDto {
     private String image;
     private int lprice;
     private int myprice;
+    private List<FolderResponseDto> productFolderList = new ArrayList<>();
 
     public ProductResponseDto(Product product) {
         this.id = product.getId();
@@ -21,5 +26,9 @@ public class ProductResponseDto {
         this.image = product.getImage();
         this.lprice = product.getLprice();
         this.myprice = product.getMyprice();
+        // 지연로딩을 사용하기 위해서는 영속성컨텍스트가 필요해서 getProducts 메서드에 @Transactional 처리 해주고, 성능 높이기 위해 readOnly처리
+        for (ProductFolder productFolder : product.getProductFolderList()) {
+            productFolderList.add(new FolderResponseDto(productFolder.getFolder()));
+        }
     }
 }
